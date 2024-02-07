24b5c0199db1629e56415302cba18d52e533e83c
WchrpWwtk <wchrp.wwtk@gmail.com>
2024-02-07 22:20:35 +0700

    Add `ApolloProvider`.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,14 @@ import { Inter } from "next/font/google";
 import "../../public/css/globals.css";
 import SearchBar from "../components/SearchBar";
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 const inter = Inter({ subsets: ["latin"] });
+
+const client = new ApolloClient({
+  uri: "https://graphql-pokemon2.vercel.app/",
+  cache: new InMemoryCache(),
+});
 
 export const metadata: Metadata = {
   title: "Search Pokemon",
@@ -20,8 +27,10 @@ export default function RootLayout({
       <body className={inter.className}>
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
           <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-            <SearchBar />
-            {children}
+            <ApolloProvider client={client}>
+              <SearchBar />
+              {children}
+            </ApolloProvider>
           </div>
         </main>
       </body>
