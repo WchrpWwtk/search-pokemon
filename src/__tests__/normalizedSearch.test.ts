17be211a51dbb2b8139ba3380c139aeb2c6ee023
WchrpWwtk <wchrp.wwtk@gmail.com>
2024-02-11 01:12:39 +0700

    Update normalized search.

--- src/__tests__/normalizedSearch.test.ts
+++ src/__tests__/normalizedSearch.test.ts
@@ -4,10 +4,16 @@ const cases = [
   [" bulbasaur ", "bulbasaur"],
   ["      bulbasaur", "bulbasaur"],
   ["bulbasaur      ", "bulbasaur"],
+  ["BULBASAUR", "bulbasaur"],
+  ["Bulbasaur", "bulbasaur"],
+  ["bULBASAUR", "bulbasaur"],
 ];
 
 describe("normalized search", () => {
-  test.each(cases)("search with whitespace to equal no whitespace", (term, result) => {
-    expect(search(term)).toBe(result);
-  });
+  test.each(cases)(
+    "search with whitespace to equal no whitespace",
+    (term, result) => {
+      expect(search(term)).toBe(result);
+    }
+  );
 });
