ab166a6416c651c34790acbd5826eefc9725bbbb
WchrpWwtk <wchrp.wwtk@gmail.com>
2024-02-10 22:41:03 +0700

    Update not found component.

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,10 +1,12 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Image from "next/image";
 
 const NotFound = () => {
   return (
-    <Box sx={{ textAlign: "center" }}>
-      <h1>{"Not found"}</h1>
+    <Box sx={{ display: "grid", textAlign: "center", gap: 2 }}>
+      <Typography variant="h3" component="div">
+        {"Not Found"}
+      </Typography>
       <Image
         src="/gang_rocket.png"
         alt="Picture of gang rocket"
